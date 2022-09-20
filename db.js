0c4ee51981f9fa2c9125d5aabc5754a3d0fbe3a2
Govind Jaiswal <govindjaiswal.knmit.edu>
2022-09-20 10:43:43 -0700

    done

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const mongoose=require('mongoose');
 const mongooseUrl="mongodb+srv://Govind1:govind@cluster0.y5tinn1.mongodb.net/noteapp";
 const connetMongo=()=>{
-    mongoose.connect(mongooseUrl,()=>console.log("conntected with mongoos"));
+    try{
+        mongoose.connect(mongooseUrl,()=>console.log("conntected with mongoos"));
+    }catch{
+        console.log("kjdfkdjk")
+    }
 }
 module.exports=connetMongo
 
